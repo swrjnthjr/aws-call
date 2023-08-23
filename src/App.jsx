19c53d22d4d100d84f2824c5799f6ed687768e05
SwarajLcAv <swaraj.nath.792@gmail.com>
2023-08-23 15:16:23 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,47 @@ import "./App.css";
 import useMasterKenisisClient from "./Hooks/masterKenisisClient.hook";
 
 function App() {
-  const { kenisisState, handleCallBtn, handleReceiveBtn, refVideo, refRemortVideo,handleStartBtn } =
+  const { kenisisState, handleCallBtn, handleReceiveBtn, refVideo, refRemortVideo, handleStartBtn, setKenisisState } =
     useMasterKenisisClient();
+
+  const VideoRenderer = () => {
+    // let audioTracks = stream.getAudioTracks();
+    const stream = kenisisState?.remoteStream
+    console.log(stream, kenisisState)
+
+    let videoTracks = stream?.getVideoTracks();
+    console.log("Video Tracking", videoTracks, videoTracks?.length);
+
+    return videoTracks?.map((video, index) => {
+      console.log("data", index, video, typeof (video))
+      const streamToShow = new MediaStream();
+      streamToShow.addTrack(video);
+
+      console.log(index, streamToShow)
+
+      return <video
+        key={index}
+        id={"Peer" + index}
+        // ref={(current) => { if (current.srcObject) current.srcObject = streamToShow }}
+        // srcObject={streamToShow}
+        ref={(ref) => {
+          if (ref) ref.srcObject = streamToShow;
+        }}
+        autoPlay
+        playsInline
+        controls />
+    })
+  }
 
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
+
+      <input type="text" name="userId" onChange={(e) => {
+        const { name, value } = e.target
+        console.log(name, value)
+        setKenisisState({ ...kenisisState, "userId": value })
+      }} />
+
       <div>
         <button onClick={() => { handleStartBtn() }}>Start</button>
       </div>
@@ -20,16 +56,25 @@ function App() {
           <p>Creator</p>
           <video
             ref={refVideo}
-            autoPlay playsInline controls
+            autoPlay
+            playsInline
+            controls
+            muted
             className="video"></video>
+
+          <p>testing video</p>
+          <div>
+            <VideoRenderer />
+          </div>
         </div>
         <div>
           <p>Receiver</p>
           <video
             ref={refRemortVideo}
-            // src="" 
             className="video"
-            autoPlay playsInline controls
+            autoPlay
+            playsInline
+            controls
           ></video>
         </div>
       </div>
--- src/Hooks/masterKenisisClient.hook.js
+++ src/Hooks/masterKenisisClient.hook.js
@@ -9,6 +9,7 @@ const useMasterKenisisClient = () => {
         remoteStream: null,
         kinesisVideoClient: null,
         audio_video: false,
+        userId: null
     });
 
     const masterState = {
@@ -57,7 +58,7 @@ const useMasterKenisisClient = () => {
         console.log('CREATING singaling channel');
         // get signaling channel arn
         const describeSignalingChannel = await kinesisVideoClient.describeSignalingChannel({
-            ChannelName: 'av-test'
+            ChannelName: `av-test_${kenisisState.userId}`
         }).promise();
         let channelARN = describeSignalingChannel.ChannelInfo.ChannelARN;
         console.log("MASTER Channle ARN", channelARN);
@@ -173,8 +174,9 @@ const useMasterKenisisClient = () => {
                 console.log("MASTER adding remote track for", remotClientId);
                 // state.setRemoteStream(event.streams[0]);
                 // ui to set view stream
+                console.log("streams2", event?.streams)
                 setKenisisState({ ...kenisisState, remoteStream: event.streams[0] })
-                refRemortVideo.current.srcObject = event.streams[0]
+                refRemortVideo.current.srcObject = event.streams?.[1]
 
                 console.log("After Setting up remote track");
             });
@@ -209,7 +211,7 @@ const useMasterKenisisClient = () => {
 
         signalingClient.on('close', () => {
             console.log('MASTER Disconnected from singaling channel');
-        });
+        })
 
         signalingClient.on('error', () => {
             console.log('MASTER Signaling client error');
@@ -253,153 +255,157 @@ const useMasterKenisisClient = () => {
     };
 
     const handleReceiveBtn = async () => {
-        const describeSignalingChannel = await kinesisVideoClient.describeSignalingChannel({
-            ChannelName: 'av-test'
-        }).promise();
+        console.log("data")
+    }
+    // const handleReceiveBtn = async () => {
+    //     const describeSignalingChannel = await kinesisVideoClient.describeSignalingChannel({
+    //         ChannelName: `av-test_${kenisisState.userId}`
+    //     }).promise();
 
-        let channelARN = describeSignalingChannel.ChannelInfo.ChannelARN;
+    //     let channelARN = describeSignalingChannel.ChannelInfo.ChannelARN;
 
-        const getSignalingChannelEndpointResponse = await kinesisVideoClient
-            .getSignalingChannelEndpoint({
-                ChannelARN: channelARN,
-                SingleMasterChannelEndpointConfiguration: {
-                    Protocols: ['WSS', 'HTTPS'],
-                    Role: KVSWebRTC.Role.VIEWER,
-                },
-            })
-            .promise();
+    //     const getSignalingChannelEndpointResponse = await kinesisVideoClient
+    //         .getSignalingChannelEndpoint({
+    //             ChannelARN: channelARN,
+    //             SingleMasterChannelEndpointConfiguration: {
+    //                 Protocols: ['WSS', 'HTTPS'],
+    //                 Role: KVSWebRTC.Role.VIEWER,
+    //             },
+    //         })
+    //         .promise();
 
-        const endpointsByProtocol = getSignalingChannelEndpointResponse.ResourceEndpointList.reduce((endpoints, endpoint) => {
-            endpoints[endpoint.Protocol] = endpoint.ResourceEndpoint;
-            return endpoints;
-        }, {});
+    //     const endpointsByProtocol = getSignalingChannelEndpointResponse.ResourceEndpointList.reduce((endpoints, endpoint) => {
+    //         endpoints[endpoint.Protocol] = endpoint.ResourceEndpoint;
+    //         return endpoints;
+    //     }, {});
 
-        const kinesisVideoSignalingChannelsClient = new AWS.KinesisVideoSignalingChannels({
-            region: 'ap-southeast-1',
-            accessKeyId: kenisisState?.secretKey,
-            secretAccessKey: kenisisState?.secretValue,
-            sessionToken: '',
-            endpoint: endpointsByProtocol.HTTPS,
-            correctClockSkew: true,
-        });
+    //     const kinesisVideoSignalingChannelsClient = new AWS.KinesisVideoSignalingChannels({
+    //         region: 'ap-southeast-1',
+    //         accessKeyId: kenisisState?.secretKey,
+    //         secretAccessKey: kenisisState?.secretValue,
+    //         sessionToken: '',
+    //         endpoint: endpointsByProtocol.HTTPS,
+    //         correctClockSkew: true,
+    //     });
 
-        const getIceServerConfigResponse = await kinesisVideoSignalingChannelsClient
-            .getIceServerConfig({
-                ChannelARN: channelARN,
-            })
-            .promise();
+    //     const getIceServerConfigResponse = await kinesisVideoSignalingChannelsClient
+    //         .getIceServerConfig({
+    //             ChannelARN: channelARN,
+    //         })
+    //         .promise();
 
-        const iceServers = [];
-        //iceServers.push({ urls: `stun:stun.kinesisvideo.ap-southeast-1.amazonaws.com:443`});
-        getIceServerConfigResponse.IceServerList.forEach((iceServer) =>
-            iceServers.push({
-                urls: iceServer.Uris,
-                username: iceServer.Username,
-                credential: iceServer.Password,
-            }))
-        let signalingClient = new KVSWebRTC.SignalingClient({
-            channelARN,
-            channelEndpoint: endpointsByProtocol.WSS,
-            clientId: Math.random()
-                .toString(36)
-                .substring(2)
-                .toUpperCase(),
-            role: KVSWebRTC.Role.VIEWER,
-            region: 'ap-southeast-1',
-            credentials: {
-                accessKeyId: kenisisState?.secretKey,
-                secretAccessKey: kenisisState?.secretValue,
-                sessionToken: '',
-            },
-            systemClockOffset: kinesisVideoClient.config.systemClockOffset,
-        });
+    //     const iceServers = [];
+    //     //iceServers.push({ urls: `stun:stun.kinesisvideo.ap-southeast-1.amazonaws.com:443`});
+    //     getIceServerConfigResponse.IceServerList.forEach((iceServer) =>
+    //         iceServers.push({
+    //             urls: iceServer.Uris,
+    //             username: iceServer.Username,
+    //             credential: iceServer.Password,
+    //         }))
+    //     let signalingClient = new KVSWebRTC.SignalingClient({
+    //         channelARN,
+    //         channelEndpoint: endpointsByProtocol.WSS,
+    //         clientId: Math.random()
+    //             .toString(36)
+    //             .substring(2)
+    //             .toUpperCase(),
+    //         role: KVSWebRTC.Role.VIEWER,
+    //         region: 'ap-southeast-1',
+    //         credentials: {
+    //             accessKeyId: kenisisState?.secretKey,
+    //             secretAccessKey: kenisisState?.secretValue,
+    //             sessionToken: '',
+    //         },
+    //         systemClockOffset: kinesisVideoClient.config.systemClockOffset,
+    //     });
 
-        const configuration = {
-            iceServers,
-            iceTransportPolicy: 'relay'
-        };
+    //     const configuration = {
+    //         iceServers,
+    //         iceTransportPolicy: 'relay'
+    //     };
 
-        const resolution = { width: { ideal: 640 }, height: { ideal: 480 } };
+    //     const resolution = { width: { ideal: 640 }, height: { ideal: 480 } };
 
-        const constraints = {
-            video: resolution,
-            audio: true,
-        };
+    //     const constraints = {
+    //         video: resolution,
+    //         audio: true,
+    //     };
 
-        let peerConnection = new RTCPeerConnection(configuration);;
+    //     let peerConnection = new RTCPeerConnection(configuration);;
 
-        viewerState.peerConnectionStatsInterval = setInterval(() => peerConnection.getStats().then((stats) => console.log("Peer connection stats", stats)), 1000);
+    //     viewerState.peerConnectionStatsInterval = setInterval(() => peerConnection.getStats().then((stats) => console.log("Peer connection stats", stats)), 1000);
 
-        signalingClient.on('open', async () => {
-            console.log('VIEWER connected to signaling service');
-            // sending local stream to other users
-            let currentState = kenisisState;
-            if (currentState.localStream) {
-                currentState.localStream.getTracks().forEach((track) => peerConnection.addTrack(track, currentState.localStream));
-            }
-            console.log('VIEWER creating SDP offer');
-            await peerConnection.setLocalDescription(
-                await peerConnection.createOffer({
-                    offerToReceiveAudio: true,
-                    offerToReceiveVideo: true,
-                }),
-            );
+    //     signalingClient.on('open', async () => {
+    //         console.log('VIEWER connected to signaling service');
+    //         // sending local stream to other users
+    //         let currentState = kenisisState;
+    //         if (currentState.localStream) {
+    //             currentState.localStream.getTracks().forEach((track) => peerConnection.addTrack(track, currentState.localStream));
+    //         }
+    //         console.log('VIEWER creating SDP offer');
+    //         await peerConnection.setLocalDescription(
+    //             await peerConnection.createOffer({
+    //                 offerToReceiveAudio: true,
+    //                 offerToReceiveVideo: true,
+    //             }),
+    //         );
 
-            if (useTrickle) {
-                console.log('viewer sending SDP offer');
-                signalingClient.sendSdpOffer(peerConnection.localDescription);
-            }
-            console.log('VIEWER generating ice candidates');
-        })
+    //         if (useTrickle) {
+    //             console.log('viewer sending SDP offer');
+    //             signalingClient.sendSdpOffer(peerConnection.localDescription);
+    //         }
+    //         console.log('VIEWER generating ice candidates');
+    //     })
 
-        signalingClient.on('sdpAnswer', async (answer) => {
-            console.log('VIEWER recieved SDP answer');
-            await peerConnection.setRemoteDescription(answer);
-        })
+    //     signalingClient.on('sdpAnswer', async (answer) => {
+    //         console.log('VIEWER recieved SDP answer');
+    //         await peerConnection.setRemoteDescription(answer);
+    //     })
 
-        signalingClient.on('iceCandidate', (candidate) => {
-            console.log('VIEWER recieved ICE candidate');
-            peerConnection.addIceCandidate(candidate);
-        })
+    //     signalingClient.on('iceCandidate', (candidate) => {
+    //         console.log('VIEWER recieved ICE candidate');
+    //         peerConnection.addIceCandidate(candidate);
+    //     })
 
-        signalingClient.on('close', () => {
-            console.log('VIEWER disconnected from singaling channel');
-        })
+    //     signalingClient.on('close', () => {
+    //         console.log('VIEWER disconnected from singaling channel');
+    //     })
 
-        signalingClient.on('error', (error) => {
-            console.log('VIEWER Signaling client error', error);
-        })
+    //     signalingClient.on('error', (error) => {
+    //         console.log('VIEWER Signaling client error', error);
+    //     })
 
-        peerConnection.addEventListener('icecandidate', ({ candidate }) => {
-            if (candidate) {
-                console.log('VIEWER Generated ICE Candidate');
-                if (useTrickle) {
-                    console.log('VIEWER Sending ICE candidate');
-                    signalingClient.sendIceCandidate(candidate);
-                }
-            } else {
-                console.log('VIEWER All ICE candidates have been generated');
-                // When trickle ICE is disabled, send the offer now that all the ICE candidates have ben generated.
-                if (!useTrickle) {
-                    console.log('VIEWER Sending SDP offer');
-                    signalingClient.sendSdpOffer(peerConnection.localDescription);
-                }
-            }
-        })
-        // Remote Stream Recieved adding it to state
-        peerConnection.addEventListener('track', (event) => {
-            console.log("VIEWER Recieved remote track");
-            // setting  UI remote stream
-            setKenisisState({ ...kenisisState, remoteStream: event.streams[0] })
-            refRemortVideo.current.srcObject = event.streams[0]
+    //     peerConnection.addEventListener('icecandidate', ({ candidate }) => {
+    //         if (candidate) {
+    //             console.log('VIEWER Generated ICE Candidate');
+    //             if (useTrickle) {
+    //                 console.log('VIEWER Sending ICE candidate');
+    //                 signalingClient.sendIceCandidate(candidate);
+    //             }
+    //         } else {
+    //             console.log('VIEWER All ICE candidates have been generated');
+    //             // When trickle ICE is disabled, send the offer now that all the ICE candidates have ben generated.
+    //             if (!useTrickle) {
+    //                 console.log('VIEWER Sending SDP offer');
+    //                 signalingClient.sendSdpOffer(peerConnection.localDescription);
+    //             }
+    //         }
+    //     })
+    //     // Remote Stream Recieved adding it to state
+    //     peerConnection.addEventListener('track', (event) => {
+    //         console.log("VIEWER Recieved remote track");
+    //         // setting  UI remote stream
+    //         console.log("streams",event.streams)
+    //         setKenisisState({ ...kenisisState, remoteStream: event.streams[0] })
+    //         refRemortVideo.current.srcObject = event.streams[0]
 
-            console.log("After setting up remote track", kenisisState);
-        })
+    //         console.log("After setting up remote track", kenisisState);
+    //     })
 
-        console.log('VIEWER Starting viewer connection');
-        viewerState.signalingClient = signalingClient;
-        signalingClient.open();
-    };
+    //     console.log('VIEWER Starting viewer connection');
+    //     viewerState.signalingClient = signalingClient;
+    //     signalingClient.open();
+    // };
 
     useEffect(() => {
         const getKeys = async () => {
@@ -415,7 +421,9 @@ const useMasterKenisisClient = () => {
         handleReceiveBtn,
         refVideo,
         refRemortVideo,
-        handleStartBtn
+        handleStartBtn,
+
+        setKenisisState
     };
 };
 
